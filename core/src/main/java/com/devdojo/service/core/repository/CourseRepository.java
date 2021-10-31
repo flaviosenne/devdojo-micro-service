d6c485c24b9ea8deefbfd7de9b2ad571a06287c6
joao flavio <flaviosenne123@gmail.com>
2021-10-31 13:43:19 -0300

    modularizar course e core

--- core/src/main/java/com/devdojo/service/core/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devdojo.service.core.repository;
-
-import com.devdojo.service.core.model.Course;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
-}
